968dc8b6676dada01f9472aaea8dce160ab75a30
Pedro H. Rocha <phr.tech@outlook.com>
2024-12-05 18:50:28 -0300

    Corrigindo API

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config();
 
 const corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
